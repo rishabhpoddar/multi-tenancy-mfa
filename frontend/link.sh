1fc4ef3447b72fbc86310205c0f9a8b1e308b7f2
rishabhpoddar <rishabh.poddar@gmail.com>
2024-03-27 16:39:59 +0530

    adds link script

---
+++ frontend/link.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# pack-unpack repo to simulate an install
+cd ../../../supertokens-auth-react/
+npm run build-pretty
+npm pack
+cd ../users/multi-tenancy-mfa/frontend/
+
+rm -rf node_modules/.cache
+rm -rf node_modules/supertokens-auth-react/lib || true
+rm -rf node_modules/supertokens-auth-react/recipe || true
+mkdir -p node_modules/supertokens-auth-react
+tar -xf ../../../supertokens-auth-react/supertokens-auth-react-*.tgz --strip-components=1 -C node_modules/supertokens-auth-react
+rm ../../../supertokens-auth-react/supertokens-auth-react-*.tgz
+
+cd node_modules/supertokens-auth-react
+
+# install prod dependencies only
+npm i --production || exit $?
+
+rm -rf node_modules/react # We need this because we do local install instead which doesn't take the parent dir into account
+rm -rf node_modules/.cache
+rm -rf node_modules/supertokens-web-js || true
+rm -rf node_modules/supertokens-website || true
+
+# We symlink the supertokens-web-js dep to ensure it's the same version (maybe linked locally)
+ln -s ../../../supertokens-auth-react/node_modules/supertokens-web-js node_modules/supertokens-web-js
+
+# We symlink the supertokens-website dep to ensure it's the same version (maybe linked locally)
+ln -s ../../../supertokens-auth-react/node_modules/supertokens-website node_modules/supertokens-website
+
+echo "$1 prepped."
